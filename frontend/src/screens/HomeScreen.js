de10a121b3f06935fb5459047a7beb4a911f92da
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-08-30 20:53:12 +0530

    Added Loading Spinner and Error Message functionalities

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,8 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 // import data from '../data';
 
 const reducer = (state, action) => {
@@ -53,9 +55,9 @@ function HomeScreen(){
             <h1>Featured Products</h1>
             <div className="products">
                 {loading ? ( 
-                    <div>Loading..</div> 
+                    <LoadingBox /> 
                     ) : error ? ( 
-                    <div>{error}</div> 
+                    <MessageBox variant="danger">{error}</MessageBox>
                     ) : (                               
                     <Row>
                         {
