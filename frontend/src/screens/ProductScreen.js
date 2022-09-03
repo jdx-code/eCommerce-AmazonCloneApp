2480d7c1e07f3d48e01154031ac46d31f37bad6a
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-09-03 20:25:28 +0530

    cartScreen component added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,7 +6,7 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
@@ -29,6 +29,7 @@ const reducer = (state, action) => {
 
 function ProductScreen() {    
 
+    const navigate = useNavigate();
     const params = useParams();
     const {slug} = params;
 
@@ -60,13 +61,14 @@ function ProductScreen() {
         const quantity = existItem ? existItem.quantity + 1 : 1;
         const { data } = await axios.get(`/api/products/${product._id}`);
         if(data.countInStock < quantity){
-            window.alert('Sorry, Product is out of stock.');
+            window.alert('Sorry, this product has no more stocks.');
             return; 
         }
         ctxDispatch({
             type: 'CART_ADD_ITEM', 
             payload: {...product, quantity}
         })
+        navigate('/cart');
     }
 
     return(
